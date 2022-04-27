0b0fe78ea348239bc4b8b493c8af5b7eaf96c905
Max Z <max_zulfer@yahoo.com>
2022-04-27 14:14:09 -0700

    quotes array

---
+++ randomQuote.js
@@ -0,0 +1,1 @@
+const randomQuotes = ['Do it now. It is not safe to leave a generous feeling to the cooling influences of the world.', 'Dreams come true. Without that possibility, nature would not incite us to have them.', 'The great thing about democracy is that it gives every voter a chance to do something stupid.', 'If you can find something everyone agrees on, it\'s wrong.']
