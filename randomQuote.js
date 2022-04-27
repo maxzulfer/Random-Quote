c29d3791222df2a574de9d7c9d0636418a9c754f
Max Z <max_zulfer@yahoo.com>
2022-04-27 16:57:13 -0700

    random number var

--- randomQuote.js
+++ randomQuote.js
@@ -1,1 +1,8 @@
 const randomQuotes = ['Do it now. It is not safe to leave a generous feeling to the cooling influences of the world.', 'Dreams come true. Without that possibility, nature would not incite us to have them.', 'The great thing about democracy is that it gives every voter a chance to do something stupid.', 'If you can find something everyone agrees on, it\'s wrong.']
+
+let randomNumber = Math.floor(Math.random() * 5)
+
+const randomQuoteGenerator = (num) => {
+    
+}
+
