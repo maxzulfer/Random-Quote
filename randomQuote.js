5bffcbf47b135544ae91d938c04f8523d44b7486
Max Z <max_zulfer@yahoo.com>
2022-04-28 23:27:19 -0700

    button functionality

--- randomQuote.js
+++ randomQuote.js
@@ -1,9 +1,12 @@
 const randomQuotes = ['Do it now. It is not safe to leave a generous feeling to the cooling influences of the world.', 'Dreams come true. Without that possibility, nature would not incite us to have them.', 'The great thing about democracy is that it gives every voter a chance to do something stupid.', 'If you can find something everyone agrees on, it\'s wrong.']
 
-const randomQuoteGenerator = () => {
+// Default Quote
+let quote = document.querySelector('q')
+quote.innerHTML = randomQuotes[0]
+
+// Random Quote Function
+let quoteButton = document.querySelector('button')
+quoteButton.onclick = () => {
     let randomNumber = Math.floor(Math.random() * 4)
-    return randomQuotes[randomNumber]
+    quote.innerHTML = randomQuotes[randomNumber]
 }
-
-console.log(randomQuoteGenerator())
-
