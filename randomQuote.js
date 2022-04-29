899cced7a3a74c6037973717f73719bb7c5aa824
Max Z <max_zulfer@yahoo.com>
2022-04-29 14:18:06 -0700

    design final

--- randomQuote.js
+++ randomQuote.js
@@ -5,12 +5,21 @@ let quote = document.querySelector('q');
 let author = document.getElementById("author");
 let btn = document.getElementById("btn");
 
+// Copy Quote
+const myInp = document.getElementById('quote').innerHTML
+const btnCopy = document.getElementById('copy-icon')
+
 let getQuote = () => {
     fetch(url)
         .then((data) => data.json())
         .then((item) => {
             quote.innerText = ' ' + item.content + ' ';
             author.innerText = '- ' + item.author;
+            btnCopy.onclick = function () {
+                item.content.select()
+                document.execCommand("Copy")
+                alert('Quote Copied to clipboard')
+            }
         });
 };
 
