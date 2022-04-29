30a4c0c29616b0145768ae5e3e94dd94ac851b8a
Max Z <max_zulfer@yahoo.com>
2022-04-29 10:27:58 -0700

    API

--- randomQuote.js
+++ randomQuote.js
@@ -1,17 +1,18 @@
-const randomQuotes = ['Do it now. It is not safe to leave a generous feeling to the cooling influences of the world.', 'Dreams come true. Without that possibility, nature would not incite us to have them.', 'The great thing about democracy is that it gives every voter a chance to do something stupid.', 'If you can find something everyone agrees on, it\'s wrong.']
+// API
+const url = "https://api.quotable.io/random";
 
-// Default Quote
-let quote = document.querySelector('q')
-quote.innerHTML = randomQuotes[0]
+let quote = document.querySelector('q');
+let author = document.getElementById("author");
+let btn = document.getElementById("btn");
 
-// Random Quote Function
-let quoteButton = document.querySelector('button')
-quoteButton.onclick = () => {
-    let randomNumber = Math.floor(Math.random() * 4)
-    quote.innerHTML = randomQuotes[randomNumber]
-}
+let getQuote = () => {
+    fetch(url)
+        .then((data) => data.json())
+        .then((item) => {
+            quote.innerText = item.content;
+            author.innerText = item.author;
+        });
+};
 
-// Date
-let date = document.getElementById('date')
-date.innerHTML = new Date().getMonth() + 1 + "/" + new Date().getDate()
-console.log(date.innerHTML)
+window.addEventListener("load", getQuote);
+btn.addEventListener("click", getQuote);
